450b905d3b041a651e44bb380aaf108c01bcb01c
Ederson Torresini <etorresini@ifsc.edu.br>
2026-05-29 11:38:31 -0300

    Adicionar asteroide na cena 3

--- client/js/scene3.js
+++ client/js/scene3.js
@@ -4,13 +4,13 @@ class scene3 extends Phaser.Scene {
   }
 
   create() {
-    const map = this.make.tilemap({ key: 'map' });
+    const map = this.make.tilemap({ key: "map" });
 
-    const fundo3_0 = map.addTilesetImage('fundo3_0', 'fundo3_0');
-    const fundo3_1 = map.addTilesetImage('fundo3_1', 'fundo3_1');
-    const fundo3_2 = map.addTilesetImage('fundo3_2', 'fundo3_2');
-    const plataformas_3 = map.addTilesetImage('plataformas3', 'plataformas3');
-    const colisao_3 = map.addTilesetImage('colisao3', 'colisao3');
+    const fundo3_0 = map.addTilesetImage("fundo3_0", "fundo3_0");
+    const fundo3_1 = map.addTilesetImage("fundo3_1", "fundo3_1");
+    const fundo3_2 = map.addTilesetImage("fundo3_2", "fundo3_2");
+    const plataformas_3 = map.addTilesetImage("plataformas3", "plataformas3");
+    const colisao_3 = map.addTilesetImage("colisao3", "colisao3");
     const tilesets = [
       fundo3_0,
       fundo3_1,
@@ -29,11 +29,11 @@ class scene3 extends Phaser.Scene {
       return indices[occurrence] ?? -1;
     };
 
-    const fundo30 = map.createLayer('fundo30', tilesets);
-    const fundo31 = map.createLayer('fundo31', tilesets);
-    const fundo32 = map.createLayer('fundo32', tilesets);
-    const detalhes = map.createLayer('detalhes', tilesets);
-    const plataformas3 = map.createLayer('plataformas3', tilesets);
+    const fundo30 = map.createLayer("fundo30", tilesets);
+    const fundo31 = map.createLayer("fundo31", tilesets);
+    const fundo32 = map.createLayer("fundo32", tilesets);
+    const detalhes = map.createLayer("detalhes", tilesets);
+    const plataformas3 = map.createLayer("plataformas3", tilesets);
 
     this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
     this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
@@ -43,11 +43,15 @@ class scene3 extends Phaser.Scene {
     this.playerJump = -520;
 
     const getSpawnPoint = () => {
-      const objectLayers = map.layers.filter((layer) => layer.type === 'objectgroup');
+      const objectLayers = map.layers.filter(
+        (layer) => layer.type === "objectgroup",
+      );
       for (const layer of objectLayers) {
         const spawnObject = layer.objects.find((obj) => {
-          const name = String(obj.name || '').toLowerCase();
-          return ['spawn', 'player', 'start'].some((term) => name.includes(term));
+          const name = String(obj.name || "").toLowerCase();
+          return ["spawn", "player", "start"].some((term) =>
+            name.includes(term),
+          );
         });
         if (spawnObject) {
           return {
@@ -60,14 +64,22 @@ class scene3 extends Phaser.Scene {
     };
 
     this.spawnPoint = getSpawnPoint();
-    this.player = this.physics.add.sprite(this.spawnPoint.x, this.spawnPoint.y, "az", 0);
+    this.player = this.physics.add.sprite(
+      this.spawnPoint.x,
+      this.spawnPoint.y,
+      "az",
+      0,
+    );
     this.player.setCollideWorldBounds(true);
     this.player.body.setSize(20, 46).setOffset(22, 16);
     this.player.setGravityY(850);
     this.player.setBounce(0);
 
     plataformas3.setCollisionByProperty({ collides: true });
-    if (!plataformas3.collideIndexes || plataformas3.collideIndexes.length === 0) {
+    if (
+      !plataformas3.collideIndexes ||
+      plataformas3.collideIndexes.length === 0
+    ) {
       plataformas3.setCollisionByExclusion([-1]);
     }
     this.physics.add.collider(this.player, plataformas3);
@@ -81,17 +93,19 @@ class scene3 extends Phaser.Scene {
 
     this.cursors = this.input.keyboard.createCursorKeys();
     this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-    this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    this.keySpace = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE,
+    );
 
-    this.add.text(10, 20, "Controle: direcional / A", {
-      fontSize: "12px",
-      fill: "#ffffff",
-      backgroundColor: "#000000",
-      padding: { x: 4, y: 4 },
-    })
+    this.add
+      .text(10, 20, "Controle: direcional / A", {
+        fontSize: "12px",
+        fill: "#ffffff",
+        backgroundColor: "#000000",
+        padding: { x: 4, y: 4 },
+      })
       .setScrollFactor(0)
       .setDepth(999);
-
 
     if (!this.anims.exists("stopped")) {
       this.anims.create({
@@ -110,11 +124,39 @@ class scene3 extends Phaser.Scene {
         repeat: -1,
       });
     }
+
+    this.asteroids = this.physics.add.group();
+
+    // Asteroid 1
+    this.asteroids.create(450, 0, "asteroids");
+
+    this.asteroids.children.iterate((asteroid) => {
+      asteroid.body.setAllowGravity(false);
+      asteroid.setVelocityY(500);
+
+      this.time.addEvent({
+        delay: 1000,
+        callback: () => {
+          asteroid.y = 0;
+        },
+      });
+    });
+
+    this.physics.add.overlap(
+      this.player,
+      this.asteroids,
+      () => {
+        this.respawnPlayer();
+      },
+      null,
+      this,
+    );
   }
 
   update() {
     // Movement logic unified with `cave` scene
-    const pad = this.input.gamepad.total > 0 ? this.input.gamepad.gamepads[0] : null;
+    const pad =
+      this.input.gamepad.total > 0 ? this.input.gamepad.gamepads[0] : null;
     let xAxis = 0;
     let jumpPressed = false;
 
@@ -127,7 +169,8 @@ class scene3 extends Phaser.Scene {
       } else if (this.cursors.right.isDown) {
         xAxis = 1;
       }
-      jumpPressed = this.cursors.up.isDown || this.keyW.isDown || this.keySpace.isDown;
+      jumpPressed =
+        this.cursors.up.isDown || this.keyW.isDown || this.keySpace.isDown;
     }
 
     this.player.setVelocityX(xAxis * this.playerSpeed);
@@ -139,7 +182,10 @@ class scene3 extends Phaser.Scene {
       this.player.play("stopped", true);
     }
 
-    if (jumpPressed && (this.player.body.blocked.down || this.player.body.touching.down)) {
+    if (
+      jumpPressed &&
+      (this.player.body.blocked.down || this.player.body.touching.down)
+    ) {
       this.player.setVelocityY(this.playerJump);
     }
   }
@@ -150,4 +196,4 @@ class scene3 extends Phaser.Scene {
   }
 }
 
-export default scene3
+export default scene3;
